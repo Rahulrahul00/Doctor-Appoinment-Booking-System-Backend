c89d913b6d7d0b439544795a330afd2b05d3f9c4
Rahul c shaji <rahulcshaji007@gmail.com>
2025-02-22 11:30:23 +0530

    Issues  with saving doctor data to the database have been resloved, and the doctor data has been successfully saved

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,8 @@
 import validator from "validator"
 import bcrypt from "bcrypt"
+import { v2 as cloudinary } from "cloudinary"
+import doctorModel from "../models/doctorsModel.js"
+
 
 
 
@@ -17,7 +20,7 @@ const addDoctor = async (req, res) => {
         }
 
         //Validating email format
-        if(!validator.isEmail()){
+        if(!validator.isEmail(email)){
             return res.json({success:false, message:"Please enter a valid email"})
         }
 
@@ -30,10 +33,33 @@ const addDoctor = async (req, res) => {
         const salt = await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(password, salt)
 
+        // upload image to cloudinary
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type:"image" })
+        const imageUrl = imageUpload.secure_url
 
+        // Data formate for the doctor
+        const doctorData = {
+            name,
+            email,
+            image:imageUrl,
+            password:hashedPassword,
+            speciality,
+            degree,
+            experience,
+            about,
+            fees,
+            address:JSON.parse(address),
+            date:Date.now()
+        }
+
+        const newDoctor = new doctorModel(doctorData) //Creating a new doctor instance with the provided data
+        await newDoctor.save()  //Saving the doctor data to the database
+
+        res.json({success: true, message: "Doctor Added"})
 
     }catch(error){
-
+       console.log(error)
+       res.json({success:false, message:error.message})
     }
 }
 
--- models/doctorsModel.js
+++ models/doctorsModel.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+
 const doctorSchema = new mongoose.Schema({
     name: { type:String, required: true},
     email: { type: String, required: true, unique: true},
@@ -9,7 +10,7 @@ const doctorSchema = new mongoose.Schema({
     degree: { type: String, required: true},
     experience: { type: String, required: true},
     about: { type: String, required: true},
-    available: { type: Boolean, required: true},
+    available: { type: Boolean, default:true},
     fees: { type: Number, required: true},
     address: {type: Object, required: true},
     date: {type: Number, required: true},
