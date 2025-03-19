3962f3c92fe6a410aeba7d403e32002d12d86c9d
Rahul c shaji <rahulcshaji007@gmail.com>
2025-03-19 12:29:41 +0530

    Created API to make payment of appointment using  razorpay

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken";
 import { v2 as cloudinary } from "cloudinary";
 import doctorModel from "../models/doctorsModel.js";
 import appointmentModel from "../models/appointmentModel.js";
-import razorpay from "razorpay"
+import razorpay from "razorpay";
 
 // API to register user
 
@@ -195,13 +195,13 @@ const listAppointment = async (req, res) => {
 
 const cancelAppointment = async (req, res) => {
   try {
-    const { userId, appointmentId } = req.body
+    const { userId, appointmentId } = req.body;
 
     const appointmentData = await appointmentModel.findById(appointmentId);
 
     //if appointment data doesn't exist
-    if(!appointmentData){
-        return res.json({success:false, message:"Appointment not Found"})
+    if (!appointmentData) {
+      return res.json({ success: false, message: "Appointment not Found" });
     }
 
     // Verify appointment user
@@ -209,32 +209,92 @@ const cancelAppointment = async (req, res) => {
       return res.json({ success: false, message: "Unauthorized action" });
     }
 
-    await appointmentModel.findByIdAndUpdate(appointmentId, {cancelled:true})
+    await appointmentModel.findByIdAndUpdate(appointmentId, {
+      cancelled: true,
+    });
 
     //Releasing Doctor slot
 
-    const {docId, slotDate, slotTime} = appointmentData
+    const { docId, slotDate, slotTime } = appointmentData;
 
-    const doctorData = await doctorModel.findById(docId) 
+    const doctorData = await doctorModel.findById(docId);
 
-    let slots_booked = doctorData.slots_booked
+    let slots_booked = doctorData.slots_booked;
 
-    slots_booked[slotDate] = slots_booked[slotDate].filter(e => e !== slotTime)
+    slots_booked[slotDate] = slots_booked[slotDate].filter(
+      (e) => e !== slotTime
+    );
 
-    await doctorModel.findByIdAndUpdate(docId, {slots_booked})
+    await doctorModel.findByIdAndUpdate(docId, { slots_booked });
 
-    res.json({success:true, message: 'Appointment Cancelled'})
-
+    res.json({ success: true, message: "Appointment Cancelled" });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
   }
 };
 
+const razorpayInstance = new razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
 // API to make payment of appointment using razorpay
 const paymentRazorpay = async (req, res) => {
-  
+  try {
+    const { appointmentId } = req.body;
+    const appointmentData = await appointmentModel.findById(appointmentId);
+
+    if (!appointmentData || appointmentData.cancelled) {
+      return res.json({
+        success: false,
+        message: "Appointment cancelled or not found",
+      });
+    }
+
+    //creating Option for razorpay payment
+    const options = {
+      amount: appointmentData.amount * 100,
+      currency: process.env.CURRENCY,
+      receipt: appointmentId,
+    };
+
+    // Creation of an order
+    const order = await razorpayInstance.orders.create(options);
+
+    res.json({ success: true, order });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
 }
+
+
+// API to verify payment of razorpay
+
+const verifyRazorpay = async (req, res) => {
+  try{
+
+    const {razorpay_order_id} = req.body
+    const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id)
+
+    console.log(orderInfo)
+    if(orderInfo.status === 'paid'){
+      await appointmentModel.findByIdAndUpdate(orderInfo.receipt, {payment:true})
+      res.json({success:true, messsage:"Payment successful"})
+    }else{
+      res.json({success:false, message:"Payment failed"})
+    }
+      
+
+  }catch(error){
+    console.log(error)
+    res.json({ success:false, message: error.message})
+  }
+}
+
+
+
 
 export {
   registerUser,
@@ -244,4 +304,6 @@ export {
   bookAppointment,
   listAppointment,
   cancelAppointment,
+  paymentRazorpay,
+  verifyRazorpay
 };
